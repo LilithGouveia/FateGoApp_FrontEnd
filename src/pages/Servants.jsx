af10e6f409a8d6cd4358da39534494189ac89087
Lilith Gouveia <lilithgouveia@outlook.com>
2022-08-03 20:16:37 -0300

    Iniciando filtragem por classe

--- src/pages/Servants.jsx
+++ src/pages/Servants.jsx
@@ -32,6 +32,56 @@ function Servants() {
   return (
     <div className='servants'>
       <Header />
+      <div className='filterClass'>
+        <p> Filter by Class </p>
+        <div className='searchClass'>
+          <img className='images' src={matchTheUrl('saber')} alt='saber' />
+          <img className='images' src={matchTheUrl('archer')} alt='archer' />
+          <img className='images' src={matchTheUrl('lancer')} alt='lancer' />
+          <img
+            className='images'
+            src={matchTheUrl('assassin')}
+            alt='assassin'
+          />
+          <img className='images' src={matchTheUrl('caster')} alt='caster' />
+          <img
+            className='images'
+            src={matchTheUrl('berserker')}
+            alt='berserker'
+          />
+          <img className='images' src={matchTheUrl('rider')} alt='rider' />
+          <img className='images' src={matchTheUrl('avenger')} alt='avenger' />
+        </div>
+        <div className='searchClass2'>
+          <img className='images' src={matchTheUrl('ruler')} alt='ruler' />
+          <img
+            className='images'
+            src={matchTheUrl('moonCancer')}
+            alt='moonCancer'
+          />
+          <img
+            className='images'
+            src={matchTheUrl('pretender')}
+            alt='pretender'
+          />
+          <img
+            className='images'
+            src={matchTheUrl('shielder')}
+            alt='shielder'
+          />
+          <img
+            className='images'
+            src={matchTheUrl('foreigner')}
+            alt='foreigner'
+          />
+          <img
+            className='images'
+            src={matchTheUrl('alterEgo')}
+            alt='alterEgo'
+          />
+          <img className='images' src={matchTheUrl('beast')} alt='beast' />
+        </div>
+      </div>
       {loading === true ? <Loading /> : null}
       {data === null ? (
         <Notfound />
@@ -52,10 +102,9 @@ function Servants() {
               </tr>
             </thead>
             <tbody>
-              {data
-                .map(
-                  ({ id, name, collectionNo, rarity, face, className }) => (
-                    <tr onClick={() => navigate(`/servants/${id}`)} key={id}>
+              {data.map(
+                ({ id, name, collectionNo, rarity, face, className }) => (
+                  <tr onClick={() => navigate(`/servants/${id}`)} key={id}>
                     <td className='id'>{collectionNo}</td>
                     <td className='container'>
                       <img className='imageProfile' src={face} alt={name} />
@@ -64,14 +113,14 @@ function Servants() {
                         className='image'
                         src={matchTheUrl(className)}
                         alt={className}
-                        />
+                      />
                     </td>
                     <td>
-                      {rarity} {<GiBarbedStar className='icon'/>}
+                      {rarity} {<GiBarbedStar className='icon' />}
                     </td>
                   </tr>
                 )
-                )}
+              )}
             </tbody>
           </table>
         </div>
